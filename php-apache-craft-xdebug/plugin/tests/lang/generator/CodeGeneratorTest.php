cd7159f4b80f4912728f5e1f700d4f3534b17d55
Kirill Smelov <kirill.smelov@jetbrains.com>
2018-09-12 18:48:01 +0300

    Tests for string literal

--- php-apache-craft-xdebug/plugin/tests/lang/generator/CodeGeneratorTest.php
+++ php-apache-craft-xdebug/plugin/tests/lang/generator/CodeGeneratorTest.php
@@ -9,6 +9,7 @@
 namespace lang\generator;
 
 use PHPUnit\Framework\TestCase;
+use workshop\lang\Compiler;
 use workshop\lang\generator\CodeGenerator;
 use workshop\lang\lexer\Lexer;
 use workshop\lang\parser\Parser;
@@ -20,4 +21,30 @@ class CodeGeneratorTest extends TestCase {
         $generateCode = (new CodeGenerator())->generateCode($fileNode);
         $this->assertEquals("<?php echo 1;\$a=1-2;\$b=1*3/4;\$c=\$a-4;", $generateCode);
     }
+
+    public function testStringLiteralSimple()
+    {
+        $this->assertEquals("<?php echo \"a\";", Compiler::compile("echo \"a\""));
+    }
+
+    public function testStringLiteralEmpty()
+    {
+        $this->assertEquals("<?php echo \"\";", Compiler::compile("echo \"\""));
+    }
+
+    public function testStringLiteralWhitespace()
+    {
+        $this->assertEquals("<?php echo \"  \";", Compiler::compile("echo \"  \""));
+    }
+
+    public function testStringLiteralDigits()
+    {
+        $this->assertEquals("<?php echo \"1\";", Compiler::compile("echo \"1\""));
+    }
+
+    public function testStringLiteralAnySymbols()
+    {
+        $this->assertEquals("<?php echo \"echo a1 + 12 - 1a2 */*/ s - asd\";", Compiler::compile("echo \"echo a1 + 12 - 1a2 */*/ s - asd\""));
+    }
+
 }
