f6c21cfe7e35787bb83f6a1bd927013bebf38828
Andrey Sokolov <andrey.sokolov@jetbrains.com>
2018-09-25 16:07:05 +0300

    disable button on submit

--- php-apache-craft-xdebug/plugin/src/assets/dist/CompilerWidget.js
+++ php-apache-craft-xdebug/plugin/src/assets/dist/CompilerWidget.js
@@ -1,8 +1,11 @@
 $(function () {
     $("#code-form").on("submit", function () {
         var formData = $("#code").val();
+        var $btn = $("#btn-eval");
+        $btn.attr("disabled",true);
         Craft.postActionRequest('phpstorm-craft-workshop/compiler/submit', {code: formData}, function (response) {
                     alert(response);
+                    $btn.removeAttr("disabled");
                 });
         return false;
 
